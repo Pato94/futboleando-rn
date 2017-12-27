91d54c54a84e297f89772f77a8c172ee56af56ee
Claudio Graiño <claudio.cuervo.17@gmail.com>
2017-12-27 12:13:03 -0300

    Match form

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import store from './src/store'
 import { Provider } from 'react-redux'
-import { MatchsList, Login } from './src/containers'
+import { Login, MatchsList, MatchForm } from './src/containers'
 
 class App extends React.Component {
   render() {
