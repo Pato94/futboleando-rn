3e1455b5f5c0379f56453d1d2701aaa2b3a5a629
Pato <Pato9406@gmail.com>
2017-12-29 11:31:42 -0300

    Add prod info

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -11,6 +11,8 @@ class Login extends React.Component {
       const result = await Expo.Google.logInAsync({
         androidClientId: "457965053885-n39fmm519fvv4v1nsa1f6no9e56ru4i4.apps.googleusercontent.com",
         iosClientId: "457965053885-lt8qi94i8hjgttfsq3t9308g9pjirk9r.apps.googleusercontent.com",
+        androidStandaloneAppClientId: "457965053885-n39fmm519fvv4v1nsa1f6no9e56ru4i4.apps.googleusercontent.com",
+        iosStandaloneAppClientId: "457965053885-lt8qi94i8hjgttfsq3t9308g9pjirk9r.apps.googleusercontent.com",
         scopes: ['email']
       });
 
